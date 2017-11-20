c1880021137b1bb1b458a31199300a5c8f0845e6
Alex <alexander.kasten@5minds.de>
2017-11-20 16:50:50 +0100

    :lipstick: Split content of interfaces.ts

---
+++ src/ievent.ts
@@ -0,0 +1,5 @@
+import {IEventMetadata} from './ievent_metadata';
+
+export interface IEvent {
+    metadata: IEventMetadata;
+}
---
+++ src/ievent_metadata.ts
@@ -0,0 +1,9 @@
+import {ExecutionContext} from '@essential-projects/core_contracts';
+
+export interface IEventMetadata {
+    id: string;
+    applicationId?: string;
+    context: ExecutionContext;
+    response?: string;
+    options?: { [key: string]: any };
+}
