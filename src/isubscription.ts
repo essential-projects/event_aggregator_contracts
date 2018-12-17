96973420027e2c299aa729c49ae81bb9bd08e7fb
ElRaptorus <christian.werner@5minds.de>
2018-12-17 12:46:19 +0100

    :lipstick: Improve some naming

--- src/isubscription.ts
+++ src/isubscription.ts
@@ -1,13 +1,13 @@
-export type EventCallback = (eventPayload: any, eventName: string) => void | Promise<void>;
+export type EventReceivedCallback = (eventPayload: any, eventName: string) => void | Promise<void>;
 
 export class Subscription {
 
   public readonly id: string;
   public readonly eventName: string;
-  public readonly callback: EventCallback;
+  public readonly callback: EventReceivedCallback;
   public readonly onlyReceiveOnce: boolean;
 
-  constructor(id: string, eventName: string, callback: EventCallback, onlyReceiveOnce: boolean = false) {
+  constructor(id: string, eventName: string, callback: EventReceivedCallback, onlyReceiveOnce: boolean = false) {
     this.id = id;
     this.eventName = eventName;
     this.callback = callback;
