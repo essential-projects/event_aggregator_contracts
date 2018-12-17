502f28d2a9036a3c794b57338b0ed0e8aad237f8
ElRaptorus <christian.werner@5minds.de>
2018-12-17 12:11:00 +0100

    :sparkles: Add onlyReceiveOnce parameter for subscribeOnce use case

--- src/isubscription.ts
+++ src/isubscription.ts
@@ -3,10 +3,12 @@ export class Subscription {
   public readonly id: string;
   public readonly eventName: string;
   public readonly callback: Function;
+  public readonly onlyReceiveOnce: boolean;
 
-  constructor(id: string, eventName: string, callback: Function) {
+  constructor(id: string, eventName: string, callback: Function, onlyReceiveOnce: boolean = false) {
     this.id = id;
     this.eventName = eventName;
     this.callback = callback;
+    this.onlyReceiveOnce = onlyReceiveOnce;
   }
 }
