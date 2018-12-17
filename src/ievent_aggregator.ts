cbb42dfc006b1b3f2c1b444a9d589600a23b8a01
ElRaptorus <christian.werner@5minds.de>
2018-12-17 11:52:00 +0100

    :fire: Remove createEntityEvent function

--- src/ievent_aggregator.ts
+++ src/ievent_aggregator.ts
@@ -1,12 +1,7 @@
 import {ISubscription} from './isubscription';
 
-import {IEvent} from './ievent';
-
 export interface IEventAggregator {
   publish(event: string | any, data?: any): void;
   subscribe(event: string | Function, callback: Function): ISubscription;
   subscribeOnce(event: string | Function, callback: Function): ISubscription;
-  createEntityEvent(data: any, source: any, metadataOptions?: {
-    [key: string]: any,
-  }): IEvent;
 }
