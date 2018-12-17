15decfe5ddc7977b8cf91de7cca163841b242b10
ElRaptorus <christian.werner@5minds.de>
2018-12-17 11:52:28 +0100

    :sparkles: Add unsubscribe function

--- src/ievent_aggregator.ts
+++ src/ievent_aggregator.ts
@@ -4,4 +4,5 @@ export interface IEventAggregator {
   publish(event: string | any, data?: any): void;
   subscribe(event: string | Function, callback: Function): ISubscription;
   subscribeOnce(event: string | Function, callback: Function): ISubscription;
+  unsubscribe(subscription: ISubscription): void;
 }
