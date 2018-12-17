ae7adaf88eadf1478267431249eada00418a3295
ElRaptorus <christian.werner@5minds.de>
2018-12-17 12:44:49 +0100

    :sparkles: Add typedef for event callback

--- src/isubscription.ts
+++ src/isubscription.ts
@@ -1,11 +1,13 @@
+export type EventCallback = (eventPayload: any, eventName: string) => void | Promise<void>;
+
 export class Subscription {
 
   public readonly id: string;
   public readonly eventName: string;
-  public readonly callback: Function;
+  public readonly callback: EventCallback;
   public readonly onlyReceiveOnce: boolean;
 
-  constructor(id: string, eventName: string, callback: Function, onlyReceiveOnce: boolean = false) {
+  constructor(id: string, eventName: string, callback: EventCallback, onlyReceiveOnce: boolean = false) {
     this.id = id;
     this.eventName = eventName;
     this.callback = callback;
